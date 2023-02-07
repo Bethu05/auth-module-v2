fa3262ce5682cd3cc70f1f284ba664e0e8e1d0d3
Bethu05 <bethwellangat05@gmail.com>
2023-02-07 22:03:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const fs = require('fs')
 const https = require('https')
 
-const key = fs.readFile('privatekey.pem')
-const cert = fs.readFile('certificate.pem')
+const key = fs.readFile('./privatekey.pem')
+const cert = fs.readFile('./certificate.pem')
 
 const app = express();
 
